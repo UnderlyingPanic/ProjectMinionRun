b0ee28a7160d35244f9e4b8399da9b8d4d1a8c9b
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-07-29 22:22:57 +0100

    Completed swordsman movement, targetting, and attacking enemies.

--- Assets/Scripts/Creep.cs
+++ Assets/Scripts/Creep.cs
@@ -6,13 +6,15 @@ public class Creep : MonoBehaviour {
 
     public float moveSpeed;
     public float attackRange;
-    public Transform targetGate;
+    public GameObject targetGate;
     public bool ownedByThisPlayer;
     
-    
-    private List<GameObject> enemiesInSights = new List<GameObject>();
-    private Transform currentTarget;
 
+    private bool attacking;
+
+    private float sightRange;
+    private List<GameObject> enemiesInSights = new List<GameObject>();
+    private GameObject currentTarget;
     private Animator animator;
 
 
@@ -20,17 +22,33 @@ public class Creep : MonoBehaviour {
 	void Start () {
         animator = GetComponent<Animator>();
         currentTarget = targetGate;
+        sightRange = GetComponent<BoxCollider>().size.x / 2;
+
+        print(sightRange);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+
+        print(attacking);
+
         animator.SetBool("isRunning", false);
-        MoveToCurrentTarget();
+        if (Vector3.Distance(this.transform.position, currentTarget.GetComponent<Collider>().ClosestPointOnBounds(transform.position)) > attackRange && !attacking)
+        {
+            MoveToCurrentTarget();
+        } else
+        {
+            animator.SetBool("isRunning", false);
+            AttackTarget(currentTarget);
+        }
     }
 
+
+    //Target Detection has to go in Fixed Update, because OnCollisionStay syncs with it.
     private void FixedUpdate()
     {
+       
         Debug.Log(name + "- " + currentTarget);
 
         //Do this RIGHT AT THE END
@@ -39,18 +57,28 @@ public class Creep : MonoBehaviour {
 
     private void MoveToCurrentTarget()
     {
-        transform.LookAt(new Vector3(currentTarget.transform.position.x, this.transform.position.y, currentTarget.transform.position.z));
+        // Smoothly Rotate to Target
+        var targetPoint = currentTarget.transform.position;
+        var targetRotation = Quaternion.LookRotation(targetPoint - transform.position, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+
+        //Move Forward (duh)
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
 
         if (moveSpeed > 0)
         {
             animator.SetBool("isRunning", true);
+        } else
+        {
+            animator.SetBool("IsRunning", false);
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-
+        //Create a list of attackable units inside the collider
+        //We need this list so we can "foreach" them.
+        //Currently we need to "foreach" them to see which is closest
         if (other.GetComponent<Pylon>())
         {
             if (other.GetComponent<Pylon>().ownedByThisPlayer != this.ownedByThisPlayer)
@@ -85,18 +113,34 @@ public class Creep : MonoBehaviour {
 
     }
 
-    private Transform ChooseTarget ()
+    private GameObject ChooseTarget ()
     {
-        Transform closestTarget = currentTarget;
+        GameObject closestTarget = currentTarget;
 
         foreach (GameObject enemy in enemiesInSights)
         {
-            if (Vector3.Distance (this.transform.position, enemy.transform.position) < Vector3.Distance (this.transform.position, closestTarget.transform.position))
+            if (Vector3.Distance(this.transform.position, enemy.GetComponent<Collider>().ClosestPointOnBounds(transform.position)) < Vector3.Distance(this.transform.position, closestTarget.transform.position) &&
+                Vector3.Distance(this.transform.position, enemy.GetComponent<Collider>().ClosestPointOnBounds(transform.position)) < sightRange)
             {
-                closestTarget = enemy.transform;
+                closestTarget = enemy;
             }
         }
 
         return closestTarget;
+    }
+
+    public void AttackTarget(GameObject target)
+    {
+        animator.SetTrigger("triggerAttackA");
+    }
+
+    //These are needed so the animator can set the variable. The variable stops us sliding while attacking.
+    public void SetAttackingTrue ()
+    {
+        attacking = true;
+    }
+    public void SetAttackingFalse()
+    {
+        attacking = false;
     }
 }
