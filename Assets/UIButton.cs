a647fccb8ab987b725d770cd0bc3ffc718613b61
NATASHA\Stefan <stefanwicks@hotmail.co.uk>
2017-08-08 22:50:22 +0100

    Added buttons for gates, but they only work once.

---
+++ Assets/UIButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour {
+
+    public Lane lane;
+    private UIManager uiManager;
+
+	// Use this for initialization
+	void Start () {
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        uiManager.OpenUI(lane);
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public GameObject canvasObject;
+    public ResearchTree[] treeArray;
+        
+	// Use this for initialization
+	void Start () {
+        
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenUI(Lane lane)
+    {
+        canvasObject.SetActive(true);
+
+        treeArray = FindObjectsOfType<ResearchTree>();
+
+        foreach (ResearchTree tree in treeArray)
+        {
+            if (tree.lane != lane)
+            {
+                tree.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    public void CloseUI()
+    {
+        canvasObject.SetActive(false);
+    }
+}
